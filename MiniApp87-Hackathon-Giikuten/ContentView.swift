6b57f1925b6fed2f1397b3556c92ca5a803abed1
Kota <maedakoutaa@gmail.com>
2022-08-18 14:04:55 +0900

    Initial Commit

---
+++ MiniApp87-Hackathon-Giikuten/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MiniApp87-Hackathon-Giikuten
+//
+//  Created by 前田航汰 on 2022/08/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MiniApp87-Hackathon-Giikuten/MiniApp87_Hackathon_GiikutenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp87_Hackathon_GiikutenApp.swift
+//  MiniApp87-Hackathon-Giikuten
+//
+//  Created by 前田航汰 on 2022/08/18.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp87_Hackathon_GiikutenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
